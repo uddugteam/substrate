5590ab823f23ea6246c8a9da931b40fa99732d56
Alexander Aglerimov <formiat95@gmail.com>
2022-02-24 19:55:59 +0300

    done: try fix tests

--- client/service/src/task_manager/tests.rs
+++ client/service/src/task_manager/tests.rs
@@ -85,7 +85,8 @@ async fn run_background_task_blocking(duration: Duration, _keep_alive: impl Any)
 }
 
 fn new_task_manager(tokio_handle: tokio::runtime::Handle) -> TaskManager {
-	TaskManager::new(tokio_handle, None).unwrap()
+	// TaskManager::new(tokio_handle, None).unwrap()
+	todo!()
 }
 
 #[test]
